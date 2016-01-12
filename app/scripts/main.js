8728ef93c8773b89cbb6ccc9e6dbacbca361865a
Tsinat Zeree <cheru@Tsinats-MacBook-Pro.local>
2016-01-11 21:21:16 -0800

    sections color change

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,6 @@
+
+/*start goto button*/
+//it will show the goto button if user scrolled 200px
 // jshint devel:true
 $(document).ready(function(){
 
@@ -5,12 +8,12 @@ var menu = document.querySelector('.about');
 //var origOffsetY = menu.offsetTop;
 
 function scroll () {
-  if ($(window).scrollTop() >= 100) {
-    $('.navbar').addClass('sticky');
-  //  $('.content').addClass('menu-padding');
+  if ($(window).scrollTop() >= 200) {
+    $('.goto-top').addClass('sticky');
+
   } else {
-    $('.navbar').removeClass('sticky');
-  //  $('.content').removeClass('menu-padding');
+    $('.goto-top').removeClass('sticky');
+
   }
 
 
@@ -19,3 +22,43 @@ function scroll () {
 document.onscroll = scroll;
 
 });
+/*end goto button*/
+
+$( "#right" ).click(function() {
+  $( ".wrapper" ).animate({ "left": "+=100%" }, "fast" );
+});
+ 
+$( "#left" ).click(function(){
+  $( ".wrapper" ).animate({ "left": "-=100%" }, "fast" );
+});
+
+/*scroll navigation*/
+
+
+  // This will select everything with the class smoothScroll
+  // This should prevent problems with carousel, scrollspy, etc...
+  $('.smoothScroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000); // The number here represents the speed of the scroll in milliseconds
+        return false;
+      }
+    }
+  });
+
+
+
+
+/*end scroll navigation*/
+
+
+
+
+
+
+
+
